68b8f17f7431d238a51e886bdd06a80a9360fd28
BlazejSzczurekk <blazej.szczurek26@gmail.com>
2026-05-27 16:19:41 +0200

    Created file structure for tests

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@pytest.fixture(scope="session")
+def config():
+    """Fixture zwracająca konfigurację z pliku .env"""
+    url = os.getenv("JELLYFIN_URL")
+    user = os.getenv("JELLYFIN_USER")
+    password = os.getenv("JELLYFIN_PASSWORD")
+    
+    # Mały test bezpieczeństwa - jeśli zapomnisz wypełnić .env, testy od razu rzucą jasny błąd
+    if not all([url, user, password]):
+        pytest.fail("Błąd: Brakuje zmiennych środowiskowych w pliku .env! Sprawdź .env.example")
+        
+    return {
+        "url": url,
+        "user": user,
+        "password": password
+    }
